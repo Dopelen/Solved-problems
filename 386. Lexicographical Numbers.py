8ad26e889d476a5cd32508feed7bc3e782c2a0a4
Oleg <141639888+Dopelen@users.noreply.github.com>
2023-11-15 19:09:46 +0300

    Update 386. Lexicographical Numbers.py
    Grammar mistake

--- 386. Lexicographical Numbers.py
+++ 386. Lexicographical Numbers.py
@@ -1,7 +1,7 @@
 """You can see the description of the task using the code specified in the title on letcode.
 This program have O(n) complexity by time
 The tricky part of this problem is "lexicographical order" in answer
-So at the beginning I use List Comprehension to create "str" and the sort it
+So at the beginning I use List Comprehension to create "str" of numbers and sort it
 Sorting will be lexicographical by default
 For answer I convert str into int"""
 
