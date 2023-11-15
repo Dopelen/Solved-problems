0280d2ddcbf600ab983d878b77ab500dac0030ed
Dopelen <oleg.ablov@gmail.com>
2023-11-15 20:58:54 +0300

    The first update

---
+++ 412. Fizz Buzz.py
@@ -0,0 +1,8 @@
+"""You can see the description of the task using the code specified in the title on letcode.
+This program have O(n) complexity by time
+
+The solution of classic problem by using List Comprehension"""
+
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        return ["FizzBuzz" if x%3 == 0 and x%5 == 0 else "Fizz" if x%3 == 0 else "Buzz" if x%5 == 0 else str(x) for x in range(1, n+1)
