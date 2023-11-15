ea50ecc2e45254b00f95f34c226e666f89b5721c
Dopelen <oleg.ablov@gmail.com>
2023-11-15 19:07:44 +0300

    The first update

---
+++ 386. Lexicographical Numbers.py
@@ -0,0 +1,11 @@
+"""You can see the description of the task using the code specified in the title on letcode.
+This program have O(n) complexity by time
+The tricky part of this problem is "lexicographical order" in answer
+So at the beginning I use List Comprehension to create "str" and the sort it
+Sorting will be lexicographical by default
+For answer I convert str into int"""
+
+class Solution:
+    def lexicalOrder(self, n: int) -> List[int]:
+        answer = sorted([str(x) for x in range(1, n+1)])
+        return list(map(int, answer))
