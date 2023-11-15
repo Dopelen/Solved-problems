0e7d014210ae242927b4e1c131d9709d7c90e800
Dopelen <oleg.ablov@gmail.com>
2023-11-15 21:51:05 +0300

    Changed description

---
+++ 2236. Root Equals Sum of Children.py
@@ -0,0 +1,11 @@
+"""You can see the description of the task using the code specified in the title on letcode.
+This program have O(1) complexity by time
+
+Check equality, return response"""
+
+
+class Solution:
+    def checkTree(self, root):
+        if root.val == root.left.val + root.right.val:
+            return True
+        return False
