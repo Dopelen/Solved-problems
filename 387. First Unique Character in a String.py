517631914b645eceefa25ee34c78b97859b4dfcc
Oleg <141639888+Dopelen@users.noreply.github.com>
2023-11-15 18:59:03 +0300

    Update 387. First Unique Character in a String.py
    change "=" to ":"

--- 387. First Unique Character in a String.py
+++ 387. First Unique Character in a String.py
@@ -1,6 +1,6 @@
 """You can see the description of the task using the code specified in the title on letcode.
  This program have O(n) complexity by time
- The idea of solution is symple. We're just adding every character into hash-table with value = position,
+ The idea of solution is symple. We're just adding every character into hash-table with value : position,
  if symbol already in dict, then symbol's value becomes "None".
  After this we check one by one symbols in input, the first elem with "value : position" in our dict will be our answer """
 
