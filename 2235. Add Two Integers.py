018c9c6f5acc23740716b45f5e49a56c8479bcb8
Dopelen <oleg.ablov@gmail.com>
2023-11-15 21:56:13 +0300

    First update

---
+++ 2235. Add Two Integers.py
@@ -0,0 +1,9 @@
+"""You can see the description of the task using the code specified in the title on letcode.
+This program have O(1) complexity by time
+
+The table of contents did not deceive you"""
+
+class Solution:
+    def sum(self, num1: int, num2: int) -> int:
+        summ = num1 + num2
+        return summ
