cc24709d16b1dcb04c5679f238270b8d278b1878
Dopelen <oleg.ablov@gmail.com>
2023-11-15 21:25:29 +0300

    The first update

--- 226. Invert Binary Tree.py
+++ 226. Invert Binary Tree.py
@@ -1,4 +1,13 @@
 """You can see the description of the task using the code specified in the title on letcode.
-This program have O(n) complexity by time
+This program have O(n) complexity by time and some depth of the recursion
 
 Here I have plural answer for easy problem"""
+
+
+class Solution:
+    def invertTree(self, root):
+        if root:
+            root.left, root.right = root.right, root.left
+            Solution.invertTree(self, root.left)
+            Solution.invertTree(self, root.right)
+        return (root)
