cdbc2f458fe22878f9a3526c9928101cf3fc82c5
Dopelen <oleg.ablov@gmail.com>
2023-11-16 17:55:03 +0300

    First update

---
+++ 1979. Find Greatest Common Divisor of Array.py
@@ -0,0 +1,15 @@
+"""You can see the description of the task using the code specified in the title on letcode.
+This program have O(n) complexity by time and does not require additional memory
+
+Finding min and max by using built-in functions and then use Euclid's theorem to find the greatest common divisor
+"""
+
+class Solution:
+    def findGCD(self, nums):
+        a, b = max(nums), min(nums)
+        if a == b:
+            return a
+        while b != 0:
+            a = a%b
+            a, b = b, a
+        return a
